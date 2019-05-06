097639b09b9d24cec765adc79028bb1d78e96a9d
hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2019-05-06 10:19:36 -0300

    projeto-06-01

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ export default class App extends Component {
   }
 
   async componentDidMount(){
+    await ListsService.push();
     await this.getLists();
   }
 
--- app/services/ApiService.js
+++ app/services/ApiService.js
@@ -1,4 +1,5 @@
 import {RNSync} from 'rnsync';
+import NetInfo from '@react-native-community/netinfo';
 
 const url = 'http://192.168.0.27:5984';
 
@@ -9,20 +10,40 @@ export class DataStore{
         this.store = new RNSync(this.dbUrl, this.dbName);
         this.store.init();
     }
+    async hasConnection(){
+        const {type} = await NetInfo.getConnectionInfo();
+        return type !== 'none';
+    }
+    async pull(){
+        if(await this.hasConnection()){
+            return this.store.replicatePull();
+        }
+        return false;
+    }
+    async push(){
+        if(await this.hasConnection()){
+            return this.store.replicatePush();
+        }
+        return false;
+    }
     async list(){
+        await this.pull();
         const docs = await this.store.find({});
         return docs;
     }
     async create(item){
         const doc = await this.store.create(item);
+        await this.push();
         return doc;
     }
     async update(doc){
         const newDoc = await this.store.update(doc.id, doc.rev, doc.body);
+        await this.push();
         return newDoc;
     }
     async remove(id){
         await this.store.delete(id);
+        await this.push();
         return id;
     }
 }
